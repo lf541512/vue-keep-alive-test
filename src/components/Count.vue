1d943d8d62d19373147fa3aa94a76476e89e1bda
lifeng <lifeng@zillionfortune.com>
2017-10-27 09:46:31 +0800

    测试keep-alive

---
+++ src/components/Count.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+   <h1>count component</h1>
+    <router-link to="/">goto home</router-link>
+  </div>
+
+</template>
+<script>
+  export default {
+//    name:'count',
+    created(){
+      console.log('count created');
+    },
+    activated(){
+      console.log('count activated');
+    }
+  }
+</script>
+<style>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <h1 > home component</h1>
+    <router-link to="count"> goto count</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  created(){
+    console.log('home created');
+  },
+  activated(){
+//    console.log('home activated');
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
